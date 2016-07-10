cf858e40df26a0c7d667b4c538656c0989b17bd7
zqliang <liangjessy@gmail.com>
2016-07-10 18:57:45 -0400

    home page + renamed pages

---
+++ assets/scripts/script.js
@@ -0,0 +1,34 @@
+var divClone;
+
+$(document).on('click', '#courses-btn', function(){ 
+		divClone = $("#search-section").clone();
+	
+		$('#search-section').html('<h3>Search for</h3>' +
+			'<form method="get" action="/search" id="search"><input name="q" type="search" size="40" placeholder="course code" /></form>' +
+			'<h3>at</h3>' + 
+			'<form method="get" action="/search" id="search"><input name="q" type="search" size="40" placeholder="university name" /></form>' +
+			'<button id="go">GO</button>' + '<br>' +
+			'<button id="cancel">Cancel</button>')
+
+
+
+});
+
+$(document).on('click', '#programs-btn', function(){ 
+		divClone = $("#search-section").clone();
+	
+		$('#search-section').html('<h3>Search for</h3>' +
+			'<form method="get" action="/search" id="search"><input name="q" type="search" size="40" placeholder="program name" /></form>' +
+			'<h3>at</h3>' + 
+			'<form method="get" action="/search" id="search"><input name="q" type="search" size="40" placeholder="university name" /></form>' +
+			'<button id="go">GO</button>' + '<br>' +
+			'<button id="cancel">Cancel</button>')
+
+
+
+});
+
+$(document).on('click', '#cancel', function(){ 
+		$("#search-section").replaceWith(divClone);
+		divClone = $("#search-section").clone();
+ });
